f6e0cf3c28d16f9339d226d015ba0c23d939062e
Garima Jain <jain.garima88@gmail.com>
2015-08-13 16:17:49 +0530

    MyformLoginHandler changed to handle login authorization.

--- src/main/java/com/mydomain/MyAuthProvier.java
+++ src/main/java/com/mydomain/MyAuthProvier.java
@@ -6,19 +6,36 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.AuthProvider;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.mongodb.morphia.Datastore;
+
+import com.mydomain.infra.ServicesFactory;
+import com.mysocial.model.User;
+
 class MyAuthProvier implements AuthProvider {
+	private String first;
+	private String last;
+	private String username;
 
 	@Override
 	public void authenticate(JsonObject json,
 			Handler<AsyncResult<io.vertx.ext.auth.User>> handler) {
 		System.out.println("Authenticating users with: " + json);
+		
 		AsyncResult<io.vertx.ext.auth.User> result = new AsyncResult<io.vertx.ext.auth.User>() {
 			public boolean succeeded() {
-				return json.getString("username").equals("admin")
-						&& json.getString("password").equals("admin123");
+				
+				boolean auth = userAuthentication(json.getString("username"),json.getString("password"));
+				return auth;
+//				return json.getString("username").equals("admin")
+//						&& json.getString("password").equals("admin123");
+				
 			}
+			
+			
 
 			public io.vertx.ext.auth.User result() {
 				return new io.vertx.ext.auth.User() {
@@ -29,7 +46,9 @@ class MyAuthProvier implements AuthProvider {
 
 					public JsonObject principal() {
 						Map<String, Object> dataMap = new HashMap<>();
-						dataMap.put("buffer", json.getString("username"));
+						dataMap.put("username", json.getString("username"));
+						dataMap.put("first", json.getString("first"));
+						dataMap.put("last", json.getString("last"));
 						JsonObject obj = new JsonObject(dataMap);
 						return obj;
 					}
@@ -56,5 +75,29 @@ class MyAuthProvier implements AuthProvider {
 			}
 		};
 		handler.handle(result);
+	}
+
+	public boolean userAuthentication(String uname,String pwd) {
+		
+		boolean authenticated = false;
+		Datastore dataStore = ServicesFactory.getMongoDB();
+		List<User> users = dataStore.createQuery(User.class).field("userName")
+				.equal(uname).asList();
+		if (users.size() == 0) {
+			System.out.println("User does not exist");
+		}
+		else {
+			System.out.println(users.get(0).getUserName());
+			if (users.get(0).getPassword().equals(pwd)){
+				System.out.println("username-->" +uname + "password-->" + pwd + "matched");
+				first = users.get(0).getFirst();
+				last = users.get(0).getLast();
+				username = uname;
+				authenticated = true;
+			}
+		}
+		
+		
+		return authenticated;
 	}
 }
